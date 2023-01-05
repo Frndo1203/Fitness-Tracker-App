2de26b4ace3a27d1f3a87458c3451146568b8079
FERNANDO OLIVEIRA <oliveirafer06@gmail.com>
2023-01-04 21:25:38 -0300

    Added second activity

--- app/src/main/java/tech/fernandooliveira/fitnesstracker/MainActivity.kt
+++ app/src/main/java/tech/fernandooliveira/fitnesstracker/MainActivity.kt
@@ -1,11 +1,24 @@
 package tech.fernandooliveira.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnIMC: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnIMC = findViewById(R.id.btn_imc)
+
+        btnIMC.setOnClickListener {
+            // navegar para a proxima tela
+            val i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+        }
     }
 }
