cbb9c1069107b163b2a6525c863323b0350e4c0e
FERNANDO OLIVEIRA <oliveirafer06@gmail.com>
2023-01-05 18:55:24 -0300

    Created first version of the business rule

--- app/src/main/java/tech/fernandooliveira/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/tech/fernandooliveira/fitnesstracker/ImcActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
 
 class ImcActivity : AppCompatActivity() {
 
@@ -25,21 +27,44 @@ class ImcActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+
+            val result = calculateBmi(weight, height)
+
+            val bmiResponseId = bmiResponse(result)
+            Toast.makeText(this, bmiResponseId, Toast.LENGTH_SHORT).show()
 
         }
 
 
     }
 
+    @StringRes
+    private fun bmiResponse(bmi: Double): Int {
+        return when (bmi) {
+            in 0.0..14.9 -> R.string.bmi_severely_low_weight
+            in 15.0..15.9 -> R.string.bmi_very_low_weight
+            in 16.0..18.4 -> R.string.bmi_very_low_weight
+            in 18.5..24.9 -> R.string.normal
+            in 25.0..29.9 -> R.string.bmi_high_weight
+            in 30.0..34.9 -> R.string.bmi_so_high_weight
+            in 35.0..39.9 -> R.string.bmi_severely_high_weight
+            in 40.0..Double.MAX_VALUE -> R.string.bmi_extreme_weight
+            else -> R.string.bmi_response_error
+        }
+    }
+
+    private fun calculateBmi(weight: Int, height: Int): Double {
+        return weight / ((height / 100.0).pow(2.0))
+    }
+
     private fun validateFields(): Boolean {
         // cant start with 0
         // cant be empty
-        val validation_condition: Boolean =
-            (editWeight.text.toString().isNotEmpty() && editHeight.text.toString()
-                .isNotEmpty() && !editWeight.text.toString()
-                .startsWith("0") && !editHeight.text.toString().startsWith("0"))
-
-        return validation_condition
+        return (editWeight.text.toString().isNotEmpty() && editHeight.text.toString()
+            .isNotEmpty() && !editWeight.text.toString()
+            .startsWith("0") && !editHeight.text.toString().startsWith("0"))
     }
 
 }
