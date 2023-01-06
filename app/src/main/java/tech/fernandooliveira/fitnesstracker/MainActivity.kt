b9205a40a9b91c3638eb6e50b2862cec7650b0ba
FERNANDO OLIVEIRA <oliveirafer06@gmail.com>
2023-01-06 19:07:06 -0300

    Implemented invoke for click listening

--- app/src/main/java/tech/fernandooliveira/fitnesstracker/MainActivity.kt
+++ app/src/main/java/tech/fernandooliveira/fitnesstracker/MainActivity.kt
@@ -1,7 +1,9 @@
 package tech.fernandooliveira.fitnesstracker
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -9,12 +11,10 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    //    private lateinit var btnIMC: LinearLayout
     private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,14 +41,26 @@ class MainActivity : AppCompatActivity() {
 
         rvMain = findViewById(R.id.rv_main)
 
-        val adapter = MainAdapter(mainItems)
+        val adapter = MainAdapter(mainItems) { id ->
+            when (id) {
+                1 -> {
+                    val intent = Intent(this@MainActivity, BmiActivity::class.java)
+                    startActivity(intent)
+                }
+                2 -> {
+                    Log.d("MainActivity", "BmrActivity")
+                }
+            }
+        }
         rvMain.adapter = adapter
 
         rvMain.layoutManager = GridLayoutManager(this, 2)
     }
 
-    private inner class MainAdapter(private val mainItems: List<MainItem>) :
-        RecyclerView.Adapter<MainViewHolder>() {
+
+    private inner class MainAdapter(
+        private val mainItems: List<MainItem>, private val listener: (Int) -> Unit
+    ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
         // 1 - What is the XML layout for each item
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
@@ -67,17 +79,20 @@ class MainActivity : AppCompatActivity() {
             return mainItems.size
         }
 
-    }
+        private inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(item: MainItem) {
+                val img: ImageView = itemView.findViewById(R.id.item_img)
+                val title: TextView = itemView.findViewById(R.id.item_title)
+                val container: LinearLayout = itemView.findViewById(R.id.item_container_bmi)
 
-    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: MainItem) {
-            val img: ImageView = itemView.findViewById(R.id.item_img)
-            val title: TextView = itemView.findViewById(R.id.item_title)
-            val container: LinearLayout = itemView.findViewById(R.id.item_container_bmi)
+                img.setImageResource(item.drawableId)
+                title.setText(item.textStringId)
+                container.setBackgroundColor(item.color)
 
-            img.setImageResource(item.drawableId)
-            title.setText(item.textStringId)
-            container.setBackgroundColor(item.color)
+                container.setOnClickListener {
+                    listener.invoke(item.id)
+                }
+            }
         }
     }
 }
