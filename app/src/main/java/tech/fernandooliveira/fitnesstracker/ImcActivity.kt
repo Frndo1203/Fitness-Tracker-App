06e15c7d705097070c4f54faccbf62ea49542ddb
FERNANDO OLIVEIRA <oliveirafer06@gmail.com>
2023-01-05 23:17:36 -0300

    Down Keyboard when windowtoken is on focus

--- app/src/main/java/tech/fernandooliveira/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/tech/fernandooliveira/fitnesstracker/ImcActivity.kt
@@ -1,6 +1,9 @@
 package tech.fernandooliveira.fitnesstracker
 
+import android.content.Context
+import android.inputmethodservice.InputMethodService
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -41,6 +44,9 @@ class ImcActivity : AppCompatActivity() {
                 .setPositiveButton(R.string.ok) { dialog, which ->
                     dialog.dismiss()
                 }.create().show()
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 
         }
 
