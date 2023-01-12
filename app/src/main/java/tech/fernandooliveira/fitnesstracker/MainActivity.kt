0ee64bf91ddd4f406d8ec98b41b9896e4e75abb5
U013961X <U013961X@xpcorretora.com.br>
2023-01-11 21:05:44 -0300

    Added BMR Functionality

--- app/src/main/java/tech/fernandooliveira/fitnesstracker/MainActivity.kt
+++ app/src/main/java/tech/fernandooliveira/fitnesstracker/MainActivity.kt
@@ -3,7 +3,6 @@ package tech.fernandooliveira.fitnesstracker
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -48,7 +47,8 @@ class MainActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
                 2 -> {
-                    Log.d("MainActivity", "BmrActivity")
+                    val intent = Intent(this@MainActivity, BmrActivity::class.java)
+                    startActivity(intent)
                 }
             }
         }
